a55364c850723882c3686190a19924e72f16bda6
richis02 <riccardo.scalco.1@studenti.unipd.it>
2025-03-31 15:33:27 +0200

    numpy

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,8 +2,8 @@ import numpy as np
 n = 5 #number of rounds
 p = 11
 
-u = [1,0,0,0,0,0,0,0]
-k = [1,0,0,0,0,0,0,0]
+u = np.array([1, 0, 0, 0, 0, 0, 0, 0])
+k = np.array([1, 0, 0, 0, 0, 0, 0, 0])
 
 def subkey_generation(k):
     k1 = [k[0], k[2], k[4], k[6]]
@@ -12,36 +12,31 @@ def subkey_generation(k):
     k4 = [k[0], k[3], k[5], k[6]]
     k5 = [k[0], k[2], k[5], k[7]]
     k6 = [k[2], k[3], k[4], k[5]]
-    return [k1, k2, k3, k4, k5, k6]
+    return np.array([
+        k1, k2, k3, k4, k5, k6
+    ])
 
-def subkey_sum(wi_1, ki):
-    k = ki + ki
-    v = []
-    for i in range(0, len(k)):
-        v.append((wi_1[i % len(wi_1)] + k[i]) % p)
-    return v
+def subkey_sum(w, ki):
+    k = np.tile(ki, 2)  # k = ki + ki
+    return (w + k[:len(w)]) % p
 
 def substitution(v):
-    y = []
-    for i in range(0, len(v)):
-        y.append(2*v[i] % p)
-    return y
+    return (2 * v) % p
 
 def transposition(y):
-    #da fare meglio con numpy
-    z = np.array(y)[[0, 1, 2, 3, 7, 6, 5, 4]]
-    return z
+    return np.array(y)[[0, 1, 2, 3, 7, 6, 5, 4]]
+
 
 def linear(z):
     A = np.array([[2, 5], [1, 7]])
     w = z.reshape(2, 4)
-    w = np.dot(A, w) % p
+    w = (A @ w) % p
     return w.flatten()
 
 subkey = subkey_generation(k)
 
 w = u.copy()
-for i in range(0, n):
+for i in range(n):
     v = subkey_sum(w, subkey[i])
     y = substitution(v)
     z = transposition(y)
@@ -51,7 +46,7 @@ print(x)
 
 #check
 x_test = [4, 0, 0, 9, 7, 0, 0, 3]
-if x == x_test:
+if np.array_equal(x, x_test):
     print("Test passed")
 else:
     print("Test failed")
